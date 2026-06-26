b5f6eb2c8af41e4d703b669e3c45bf4770a7243e
vaishnaviwakodikar <vaishnaviwakodikar09@gmail.com>
2026-06-26 22:36:15 +0530

    Landing page - Initial

---
+++ Lawctopus/src/App.jsx
@@ -0,0 +1,13 @@
+
+import LawctopusLanding from "./pages/Landingpage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LawctopusLanding />
+    </div>
+  );
+}
+
+export default App;
